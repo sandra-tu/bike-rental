f4c8892af2b57ace037d312fa3ca7672f96fd2bf
Sandra Tu <sandratu2000@gmail.com>
2019-11-13 21:35:47 +0000

    updated TestLocation

--- tests/uk/ac/ed/bikerental/TestLocation.java
+++ tests/uk/ac/ed/bikerental/TestLocation.java
@@ -1,11 +1,7 @@
 package uk.ac.ed.bikerental;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +12,6 @@ public class TestLocation {
     @BeforeEach
     void setUp() throws Exception{
         // TODO: setup some resources before each test
-        System.out.println("what");
     	this.loc1 = new Location("EH15 7TT", "ookyspooky");
     	this.loc2 = new Location("EH5 4NG", "hmmm");
     	this.loc3 = new Location("GB12 6FF", "longwalk");
@@ -25,7 +20,6 @@ public class TestLocation {
     // TODO: put some tests here
     @Test
     public void testIsNearTo() {
-        System.out.println("hey");
         boolean close = loc1.isNearTo(loc2);
         boolean far = loc1.isNearTo(loc3);
         assertEquals(true, close);
