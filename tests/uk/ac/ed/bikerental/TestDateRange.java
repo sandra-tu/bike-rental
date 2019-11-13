fe1acdbfbc679cd8fc5cc2e81cafe5f7919afd2f
Claire Squires <clairesquires99@outlook.com>
2019-11-13 11:10:29 +0000

    Test cases added

--- tests/uk/ac/ed/bikerental/TestDateRange.java
+++ tests/uk/ac/ed/bikerental/TestDateRange.java
@@ -8,10 +8,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestDateRange {
-    private DateRange dateRange1, dateRange2, dateRange3;
+    private DateRange dateRange1, dateRange2, dateRange3, dateRange4,
+        dateRange5, dateRange7, dateRange8;
 
     @BeforeEach
-    void setUp() throws Exception {
+    void setUp() throws Exception { //Maybe it throws and exception if it can't create these objects
         // Setup resources before each test
         this.dateRange1 = new DateRange(LocalDate.of(2019, 1, 7),
                 LocalDate.of(2019, 1, 10));
@@ -19,6 +20,14 @@ class TestDateRange {
                 LocalDate.of(2019, 1, 23));
         this.dateRange3 = new DateRange(LocalDate.of(2015, 1, 7),
                 LocalDate.of(2018, 1, 10));
+        this.dateRange4 = new DateRange(LocalDate.of(2020, 1, 1),
+                LocalDate.of(2020, 1, 10));
+        this.dateRange5 = new DateRange(LocalDate.of(2020, 2, 19),
+                LocalDate.of(2020, 2, 8));
+        this.dateRange7 = new DateRange(LocalDate.of(2020, 1, 7),
+                LocalDate.of(2020, 1, 7));
+        this.dateRange8 = new DateRange(LocalDate.of(2020, 1, 7), //Check whether this is then okay
+                LocalDate.of(2019, 1, 10));
     }
 
     // Sample JUnit tests checking toYears works
@@ -35,14 +44,25 @@ class TestDateRange {
     @Test
     void testOverlapsTrue() {
         // TODO: check we can see when two date ranges overlap
-        fail();
+        assertTrue(this.dateRange1.overlaps(this.dateRange2));
+        assertTrue(this.dateRange7.overlaps(this.dateRange7));
+        assertTrue(this.dateRange4.overlaps(this.dateRange4));
+        assertTrue(this.dateRange4.overlaps(this.dateRange6));
     }
 
     @Test
     void testOverlapsFalse() {
         // TODO: check we can see when two date ranges  don't overlap
-        fail();
+        assertFalse(this.dateRange2.overlaps(this.dateRange3));
+        assertFalse(this.dateRange4.overlaps(this.dateRange5));
+        assertFalse(this.dateRange5.overlaps(this.dateRange4));
+        assertFalse(this.dateRange6.overlaps(this.dateRange5));
     }
 
     // TODO: put some of your own unit tests here
+    
+    @Test
+    void testEndBeforeStartDate() {
+        //Use dateRange8
+    }
 }
