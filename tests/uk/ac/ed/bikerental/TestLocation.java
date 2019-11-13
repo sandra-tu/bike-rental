de9ad3fb976a8fb12a5477c11b76a1a003928ca2
Sandra Tu <sandratu2000@gmail.com>
2019-11-13 19:42:46 +0000

    updated TestLocation

--- tests/uk/ac/ed/bikerental/TestLocation.java
+++ tests/uk/ac/ed/bikerental/TestLocation.java
@@ -2,6 +2,7 @@ package uk.ac.ed.bikerental;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -10,16 +11,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestLocation {
-	private Location loc1, loc2, loc3;
+	private Location loc1, loc2, loc3, loc4;
 	
     @BeforeEach
     void setUp() throws Exception{
+        // TODO: setup some resources before each test
         System.out.println("what");
     	this.loc1 = new Location("EH15 7TT", "ookyspooky");
     	this.loc2 = new Location("EH5 4NG", "hmmm");
     	this.loc3 = new Location("GB12 6FF", "longwalk");
     }
     
+    // TODO: put some tests here
     @Test
     public void testIsNearTo() {
         System.out.println("hey");
@@ -27,5 +30,16 @@ public class TestLocation {
         boolean far = loc1.isNearTo(loc3);
         assertEquals(true, close);
         assertEquals(false, far);
+    }
+    
+    @Test
+    public void testExpectedAssertions() {
+        Assertions.assertThrows(AssertionError.class, () -> {
+            this.loc4 = new Location("1234 231", "wrong");
+        });
+        
+        Assertions.assertThrows(AssertionError.class, () -> {
+            this.loc4 = new Location("123", "wrong");
+        });
     }
 }
