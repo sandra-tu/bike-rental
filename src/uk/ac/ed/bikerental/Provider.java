9ad46e6def9bc40af1897ccaca5c0d639e3e62ed
Sandra Tu <sandratu2000@gmail.com>
2019-11-14 19:12:44 +0000

    added Provider

---
+++ src/uk/ac/ed/bikerental/Provider.java
@@ -0,0 +1,36 @@
+package uk.ac.ed.bikerental;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.HashMap;
+
+public class Provider {
+    
+    private int providerID;
+    private String providerName;
+    private Location providerAddress;
+    private Set<Provider> partnerProviders;
+    private Set<Bike> stock;
+    private HashMap<BikeType, BigDecimal> dailyRentalPrice; //change from dailyPrice to dailyRentalPrice in UML cwk2
+    
+    public Provider() {
+        
+    }
+    
+    public int getProviderID() {
+        return this.providerID;
+    }
+    
+    public String getProviderName() {
+        return this.providerName;
+    }
+    
+    public Location getProviderAddress() {
+        return this.providerAddress;
+    }
+    
+    public HashMap<BikeType, BigDecimal> getDailyRentalPrice(){
+        return this.dailyRentalPrice;
+    }
+    
+}
