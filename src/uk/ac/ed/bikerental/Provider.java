98c6661e9dfb22a4c77a1a01cf20427e0325bef7
Sandra Tu <sandratu2000@gmail.com>
2019-11-19 12:56:58 +0000

    updated Provider

--- src/uk/ac/ed/bikerental/Provider.java
+++ src/uk/ac/ed/bikerental/Provider.java
@@ -20,13 +20,11 @@ public class Provider {
     private static AtomicLong idCounter = new AtomicLong();
     
     //Recheck constructor given rental price constructor
-    public Provider(String providerName, Location providerAddress, BigDecimal depositRate,
-            HashMap<BikeType, BigDecimal> dailyRentalPrice, Set<Provider> partnerProviders) {
+    public Provider(String providerName, Location providerAddress, BigDecimal depositRate, Set<Provider> partnerProviders) {
         this.providerID = createProviderID();
         this.providerName = providerName;
         this.providerAddress = providerAddress;
         this.depositRate = depositRate;
-        this.dailyRentalPrice = dailyRentalPrice;
         this.partnerProviders = partnerProviders;
     }
     
@@ -51,29 +49,18 @@ public class Provider {
     }
     
   //Should be final only if not null (how do we do that???)
-    public HashMap<BikeType, BigDecimal> setDailyRentalPrices(BigDecimal roadBRentalPrice,
-        BigDecimal mountainBRentalPrice, 
-        BigDecimal hybridBRentalPrice, 
-        BigDecimal eBRentalPrice,
-        BigDecimal otherBRentalPrice) {
+    public HashMap<BikeType, BigDecimal> setDailyRentalPrice(BikeType bikeType, BigDecimal price) {
         
-        HashMap<BikeType, BigDecimal> DailyRentalPrices = new HashMap<>();
+        HashMap<BikeType, BigDecimal> dailyRentalPrice = new HashMap<>();
         
         //Is there a way to automatically iterate through all values of enum class?
         //If BikeType no longer needs RentalPrice attribute then this chunk can be deleted
-        BikeType roadBike = new BikeType(BikeTypes.ROADBIKE, roadBRentalPrice);
-        BikeType mountainBike = new BikeType(BikeTypes.MOUNTAINBIKE, mountainBRentalPrice);
-        BikeType hybridBike = new BikeType(BikeTypes.HYBRIDBIKE, hybridBRentalPrice);
-        BikeType eBike = new BikeType(BikeTypes.EBIKE, eBRentalPrice);
-        BikeType otherBike = new BikeType(BikeTypes.OTHERBIKE, otherBRentalPrice);
-        
-        DailyRentalPrices.put(mountainBike, mountainBike.getDailyRentalPrice());
-        DailyRentalPrices.put(roadBike, roadBike.getDailyRentalPrice());
-        DailyRentalPrices.put(hybridBike, hybridBike.getDailyRentalPrice());
-        DailyRentalPrices.put(eBike, eBike.getDailyRentalPrice());
-        DailyRentalPrices.put(otherBike, otherBike.getDailyRentalPrice());
-
-        return DailyRentalPrices;
+        if(!dailyRentalPrice.containsKey(bikeType)){
+            dailyRentalPrice.put(bikeType, price);
+        } else {
+            dailyRentalPrice.replace(bikeType, price);
+        }
+        return dailyRentalPrice;
     }
     
     //Returns the daily rental price for a given bike type
