f0c4b729f4cf6cfebf280277849f9f6d471c2d78
clairesquires99 <clairesquires99@outlook.com>
2019-11-26 22:37:38 +0000

    System tests chagned all to 'this' keyword more objects

--- tests/uk/ac/ed/bikerental/SystemTests.java
+++ tests/uk/ac/ed/bikerental/SystemTests.java
@@ -19,6 +19,21 @@ import java.util.TreeSet;
 
 public class SystemTests {
     // You can add attributes here
+    private BikeType mountainBike, roadBike, eBike, otherBike;
+    private Location locationP1, locationP2, locationP3, locationP4;
+    private Location locationC1, locationC2, locationC3, locationC4;
+    private DateRange dateRange1, dateRange2;
+    private Provider provider1, provider2, provider3, provider4;
+    
+    private Bike bike1_1, bike1_2, bike1_3, bike1_4;
+    private Bike bike2_1, bike2_2, bike2_3, bike2_4, bike2_5, bike2_6, bike2_7;
+    private Bike bike3_1, bike3_2, bike3_3, bike3_4, bike3_5;
+    private Bike bike4_1, bike4_2, bike4_3, bike4_4, bike4_5, bike4_6, bike4_7, bike4_8;
+    
+    private ArrayList<BikeType> requestedBikes1, requestedBikes2, requestedBikes3;
+    private Input input1, input2, input3, input4, input5, input6;
+    private ArrayList<Quote> quotes1;
+    
 
     @BeforeEach
     void setUp() throws Exception {
@@ -28,113 +43,116 @@ public class SystemTests {
         // Put your test setup here
         
         //Bike types
-        BikeType mountainBike = new BikeType(BikeTypes.MOUNTAINBIKE, new BigDecimal(100.00));
-        BikeType roadBike = new BikeType(BikeTypes.ROADBIKE, new BigDecimal(110.00));
-        BikeType eBike = new BikeType(BikeTypes.EBIKE, new BigDecimal(120.00));
-        BikeType otherBike = new BikeType(BikeTypes.OTHERBIKE, new BigDecimal(130.00));
+        this.mountainBike = new BikeType(BikeTypes.MOUNTAINBIKE, new BigDecimal(100.00));
+        this.roadBike = new BikeType(BikeTypes.ROADBIKE, new BigDecimal(110.00));
+        this.eBike = new BikeType(BikeTypes.EBIKE, new BigDecimal(120.00));
+        this.otherBike = new BikeType(BikeTypes.OTHERBIKE, new BigDecimal(130.00));
 
         //Locations
         //(for Provider addresses)
-        Location locationP1 = new Location("EH89QE", "Street name");
-        Location locationP2 = new Location("EH165AY", "Street name");
-        Location locationP3 = new Location("G138AB", "Street name");
-        Location locationP4 = new Location("KY144AA", "Street name");
+        this.locationP1 = new Location("EH89QE", "Street name");
+        this.locationP2 = new Location("EH165AY", "Street name");
+        this.locationP3 = new Location("G138AB", "Street name");
+        this.locationP4 = new Location("KY144AA", "Street name");
         //(for customer searches)
-        Location locationC1 = new Location("EH45AH", "Street name");
-        Location locationC2 = new Location("G149KL", "Street name");
-        Location locationC3 = new Location("KY12AB", "Street name");
-        Location locationC4 = new Location("LD12AH", "Street name");
+        this.locationC1 = new Location("EH45AH", "Street name");
+        this.locationC2 = new Location("G149KL", "Street name");
+        this.locationC3 = new Location("KY12AB", "Street name");
+        this.locationC4 = new Location("LD12AH", "Street name");
         
         //Date ranges
-        DateRange dateRange1 = new DateRange(LocalDate.of(2019, 1, 1), LocalDate.of(2019, 1, 3));
-        DateRange dateRange2 = new DateRange(LocalDate.of(2019, 1, 1), LocalDate.of(2019, 5, 1));
+        this.dateRange1 = new DateRange(LocalDate.of(2019, 1, 1), LocalDate.of(2019, 1, 3));
+        this.dateRange2 = new DateRange(LocalDate.of(2019, 1, 1), LocalDate.of(2019, 5, 1));
         
         //Providers
-        Provider provider1 = new Provider("Provider1", locationP1, new BigDecimal(0.2), null);
-        Provider provider2 = new Provider("Provider2", locationP2, new BigDecimal(0.15), null);
-        Provider provider3 = new Provider("Provider3", locationP3, new BigDecimal(0.1), null);
-        Provider provider4 = new Provider("Provider4", locationP4, new BigDecimal(0.25), null);
+        this.provider1 = new Provider("Provider1", locationP1, new BigDecimal(0.2), null);
+        this.provider2 = new Provider("Provider2", locationP2, new BigDecimal(0.15), null);
+        this.provider3 = new Provider("Provider3", locationP3, new BigDecimal(0.1), null);
+        this.provider4 = new Provider("Provider4", locationP4, new BigDecimal(0.25), null);
 
         //Setup partner providers
         Set<Provider> partnersOf1 = new HashSet<>();
-        partnersOf1.add(provider3);
+        partnersOf1.add(this.provider3);
         Set<Provider> partnersOf3 = new HashSet<>();
-        partnersOf3.add(provider1); 
-        partnersOf3.add(provider4);
+        partnersOf3.add(this.provider1); 
+        partnersOf3.add(this.provider4);
         Set<Provider> partnersOf4 = new HashSet<>();
-        partnersOf4.add(provider3);
+        partnersOf4.add(this.provider3);
         
-        provider1.setPartnerProviders(partnersOf1);
-        provider3.setPartnerProviders(partnersOf3);
-        provider4.setPartnerProviders(partnersOf4);
+        this.provider1.setPartnerProviders(partnersOf1);
+        this.provider3.setPartnerProviders(partnersOf3);
+        this.provider4.setPartnerProviders(partnersOf4);
 
         //Bikes and adding to stock
-        Bike bike1_1 = new Bike(provider1, mountainBike);
-        Bike bike1_2 = new Bike(provider1, mountainBike);
-        Bike bike1_3 = new Bike(provider1, mountainBike);
-        Bike bike1_4 = new Bike(provider1, mountainBike);
+        this.bike1_1 = new Bike(provider1, mountainBike);
+        this.bike1_2 = new Bike(provider1, mountainBike);
+        this.bike1_3 = new Bike(provider1, mountainBike);
+        this.bike1_4 = new Bike(provider1, mountainBike);
         Bike arrayProvider1[] = {bike1_1, bike1_2, bike1_3, bike1_4};
         Set<Bike> provider1Stock = new HashSet<>(Arrays.asList(arrayProvider1));
-        provider1.setStock(provider1Stock);
+        this.provider1.setStock(provider1Stock);
         
-        Bike bike2_1 = new Bike(provider2, roadBike);
-        Bike bike2_2 = new Bike(provider2, roadBike);
-        Bike bike2_3 = new Bike(provider2, roadBike);
-        Bike bike2_4 = new Bike(provider2, roadBike);
-        Bike bike2_5 = new Bike(provider2, eBike);
-        Bike bike2_6 = new Bike(provider2, eBike);
-        Bike bike2_7 = new Bike(provider2, eBike);
+        this.bike2_1 = new Bike(provider2, roadBike);
+        this.bike2_2 = new Bike(provider2, roadBike);
+        this.bike2_3 = new Bike(provider2, roadBike);
+        this.bike2_4 = new Bike(provider2, roadBike);
+        this.bike2_5 = new Bike(provider2, eBike);
+        this.bike2_6 = new Bike(provider2, eBike);
+        this.bike2_7 = new Bike(provider2, eBike);
         Bike arrayProvider2[] = {bike2_1, bike2_2, bike2_3, bike2_4, bike2_5, bike2_6, bike2_7};
         Set<Bike> provider2Stock = new HashSet<>(Arrays.asList(arrayProvider2));
-        provider2.setStock(provider2Stock);
+        this.provider2.setStock(provider2Stock);
 
-        Bike bike3_1 = new Bike(provider3, mountainBike);
-        Bike bike3_2 = new Bike(provider3, roadBike);
-        Bike bike3_3 = new Bike(provider3, roadBike);
-        Bike bike3_4 = new Bike(provider3, eBike);
-        Bike bike3_5 = new Bike(provider3, eBike);
+        this.bike3_1 = new Bike(provider3, mountainBike);
+        this.bike3_2 = new Bike(provider3, roadBike);
+        this.bike3_3 = new Bike(provider3, roadBike);
+        this.bike3_4 = new Bike(provider3, eBike);
+        this.bike3_5 = new Bike(provider3, eBike);
         Bike arrayProvider3[] = {bike3_1, bike3_2, bike3_3, bike3_4, bike3_5};
         Set<Bike> provider3Stock = new HashSet<>(Arrays.asList(arrayProvider3));
-        provider3.setStock(provider3Stock);
+        this.provider3.setStock(provider3Stock);
         
-        Bike bike4_1 = new Bike(provider4, mountainBike);
-        Bike bike4_2 = new Bike(provider4, mountainBike);
-        Bike bike4_3 = new Bike(provider4, mountainBike);
-        Bike bike4_4 = new Bike(provider4, mountainBike);
-        Bike bike4_5 = new Bike(provider4, eBike);
-        Bike bike4_6 = new Bike(provider4, eBike);
-        Bike bike4_7 = new Bike(provider4, eBike);
-        Bike bike4_8 = new Bike(provider4, eBike);
+        this.bike4_1 = new Bike(provider4, mountainBike);
+        this.bike4_2 = new Bike(provider4, mountainBike);
+        this.bike4_3 = new Bike(provider4, mountainBike);
+        this.bike4_4 = new Bike(provider4, mountainBike);
+        this.bike4_5 = new Bike(provider4, eBike);
+        this.bike4_6 = new Bike(provider4, eBike);
+        this.bike4_7 = new Bike(provider4, eBike);
+        this.bike4_8 = new Bike(provider4, eBike);
         Bike arrayProvider4[] = {bike4_1, bike4_2, bike4_3, bike4_4, bike4_5, bike4_6, 
                 bike4_7, bike4_8};
         Set<Bike> provider4Stock = new HashSet<>(Arrays.asList(arrayProvider4));
-        provider4.setStock(provider4Stock);
+        this.provider4.setStock(provider4Stock);
         
         //ArrayList of BikeType for Input
-        ArrayList<BikeType> requestedBikes1 = new ArrayList<>();
-        requestedBikes1.add(mountainBike);
+        this.requestedBikes1 = new ArrayList<>();
+        this.requestedBikes1.add(mountainBike);
         
-        ArrayList<BikeType> requestedBikes2 = new ArrayList<>();
-        requestedBikes2.add(mountainBike);
-        requestedBikes2.add(mountainBike);
-        requestedBikes2.add(mountainBike);
+        this.requestedBikes2 = new ArrayList<>();
+        this.requestedBikes2.add(mountainBike);
+        this.requestedBikes2.add(mountainBike);
+        this.requestedBikes2.add(mountainBike);
 
-        ArrayList<BikeType> requestedBikes3 = new ArrayList<>();
-        requestedBikes3.add(mountainBike);
-        requestedBikes3.add(roadBike);
-        requestedBikes3.add(eBike);
+        this.requestedBikes3 = new ArrayList<>();
+        this.requestedBikes3.add(mountainBike);
+        this.requestedBikes3.add(roadBike);
+        this.requestedBikes3.add(eBike);
         
         //Inputs
-        Input input1 = new Input(dateRange1, requestedBikes1, locationC1);
-        Input input2 = new Input(dateRange1, requestedBikes1, locationC4); //Should return no quotes
-        Input input3 = new Input(dateRange2, requestedBikes2, locationC1);
-        Input input4 = new Input(dateRange1, requestedBikes3, locationC1);
-        Input input5 = new Input(dateRange1, requestedBikes2, locationC2);
-        Input input6 = new Input(dateRange1, requestedBikes3, locationC3);
+        this.input1 = new Input(dateRange1, this.requestedBikes1, locationC1);
+        this.input2 = new Input(dateRange1, this.requestedBikes1, locationC4); //Should return no quotes
+        this.input3 = new Input(dateRange2, this.requestedBikes2, locationC1);
+        this.input4 = new Input(dateRange1, this.requestedBikes3, locationC1);
+        this.input5 = new Input(dateRange1, this.requestedBikes2, locationC2);
+        this.input6 = new Input(dateRange1, this.requestedBikes3, locationC3);
         
         //Generated Quotes
-        Set<Quote> quotes1 = new HashSet<>();
-        quotes1 = generateQuotes(input1);
+        Controller c = new Controller();
+        this.quotes1 = c.generateQuotes(input1);
+        
+        //Quotes
+        
 
     }
     
@@ -144,5 +162,20 @@ public class SystemTests {
     void myFirstTest() {
         // JUnit tests look like this
         assertEquals("The moon", "cheese"); // Should fail
+        
     }
+    
+    //Test: Use Case 1 - Finding a quote
+    
+    //Test: Use Case 2 - Booking a quote
+//    Place a booking 
+//    Unique booking number
+//    If customer has requested DeliveryService then should return
+    
+    //Test invoice details
+    
+    
+    
+    
+    //Test: Use Case 3 - Returning bikes
 }
