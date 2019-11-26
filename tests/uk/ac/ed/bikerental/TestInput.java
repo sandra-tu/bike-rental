62ddd753fdd6b9bf9f93fa206343121e60091dd4
clairesquires99 <clairesquires99@outlook.com>
2019-11-26 11:20:32 +0000

    Adapted InputTest for chagne to DateRange argument

--- tests/uk/ac/ed/bikerental/TestInput.java
+++ tests/uk/ac/ed/bikerental/TestInput.java
@@ -8,21 +8,22 @@ import org.junit.Test;
 import uk.ac.ed.bikerental.BikeType.BikeTypes;
 
 public class TestInput {
-    private Input input1, input2, input3, input4, input5;
+    private Input input1, input2, input3, input4;
     
     //Is the duplicated code in the two tests necessary? Or is there some way to avoid that?
     
-//    @BeforeEach
-//    void setUp() throws Exception {
+    @BeforeEach
+    void setUp() throws Exception {
 //        BikeType mountainBike = new BikeType(BikeTypes.MOUNTAINBIKE, new BigDecimal(20.00));
 //        BikeType roadBike = new BikeType(BikeTypes.ROADBIKE, new BigDecimal(15.00));
 //        BikeType[] bikesRequested1 = {mountainBike};
 //        BikeType[] bikesRequested2 = {mountainBike, mountainBike, roadBike};
 //        Location location = new Location("Eh165AY", "Street name");
-//    }
+    }
     
     @Test
-    public void constructorTest() {
+    public void constructorTest() throws Exception {    
+        DateRange dateRange = new DateRange(LocalDate.of(2019, 1, 7), LocalDate.of(2019, 1, 10));
         BikeType mountainBike = new BikeType(BikeTypes.MOUNTAINBIKE, new BigDecimal(20.00));
         BikeType roadBike = new BikeType(BikeTypes.ROADBIKE, new BigDecimal(15.00));
         ArrayList<BikeType> bikesRequested1 = new ArrayList<BikeType>(
@@ -32,25 +33,19 @@ public class TestInput {
                               mountainBike, 
                               roadBike));        
         Location location = new Location("EH165AY", "Street name");
-        
-        input1 = new Input(LocalDate.of(2019, 1, 7), LocalDate.of(2019, 1, 10),
-                bikesRequested1, location);
-        input2 = new Input(LocalDate.of(2019, 5, 10), LocalDate.of(2019, 6, 3),
-                bikesRequested2, location);
+        this.input1 = new Input(dateRange, bikesRequested1, location);
+        this.input2 = new Input(dateRange, bikesRequested2, location);
     }
     
     @Test(expected = IllegalArgumentException.class)
     public void constructorExceptionTest() {
+        DateRange dateRange = new DateRange(LocalDate.of(2019, 1, 7), LocalDate.of(2019, 1, 10));
         BikeType mountainBike = new BikeType(BikeTypes.MOUNTAINBIKE, new BigDecimal(20.00));
         ArrayList<BikeType> bikesRequested1 = new ArrayList<BikeType>(
                 Arrays.asList(mountainBike));
         Location location = new Location("EH165AY", "Street name");
-        
-        input3 = new Input(LocalDate.of(2019, 1, 7), LocalDate.of(2019, 1, 1),
-                bikesRequested1, location);
-        input4 = new Input(LocalDate.of(2019, 1, 7), LocalDate.of(2019, 1, 10),
-                null, location);
-        input5 = new Input(LocalDate.of(2019, 1, 7), LocalDate.of(2019, 1, 10),
-                bikesRequested1, null);
+    
+        this.input3 = new Input(dateRange, null, location);
+        this.input4 = new Input(dateRange, bikesRequested1, null);
     }
 }
