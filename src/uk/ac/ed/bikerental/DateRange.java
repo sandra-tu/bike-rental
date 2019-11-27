a651021b319ee451fddfb5e6de4c9da584b253b5
Sandra Tu <sandratu2000@gmail.com>
2019-11-27 16:15:19 +0000

    added JavaDoc comments for DateRange

--- src/uk/ac/ed/bikerental/DateRange.java
+++ src/uk/ac/ed/bikerental/DateRange.java
@@ -5,30 +5,69 @@ import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 import java.util.function.BooleanSupplier;
 
+/**
+ * Represents a range of dates, uses two LocalDate objects for start date and end date
+ */
+
 public class DateRange {
+    /**
+     * Represents the start date and end date of a date range
+     */
+    
     private LocalDate start, end;
     
+    /**
+     * Class constructor
+     * @param start LocalDate object for start date
+     * @param end   LocalDate object for end date
+     */
     public DateRange(LocalDate start, LocalDate end) {
         this.start = start;
         this.end = end;
         assert !this.start.isAfter(this.end);
     }
     
+    /**
+     * Getter method for start
+     * @return the start date of the DateRange
+     */
+    
     public LocalDate getStart() {
         return this.start;
     }
     
+    /**
+     * Getter method for end
+     * @return the end date of the DateRange
+     */
+    
     public LocalDate getEnd() {
         return this.end;
     }
+    
+    /**
+     * Method for seeing the number of years the DateRange spans
+     * @return a long representing the number of years
+     */
 
     public long toYears() {
         return ChronoUnit.YEARS.between(this.getStart(), this.getEnd());
     }
+    
+    /**
+     * Method for seeing the number of days the DateRange spans
+     * @return a long representing the number of days
+     */
 
     public long toDays() {
         return ChronoUnit.DAYS.between(this.getStart(), this.getEnd());
     }
+    
+    /**
+     * Method to check if this DateRange overlaps with another given DateRange
+     * @param other the other DateRange object to check for overlaps with
+     * @return      a boolean representing whether there is an overlap or not
+     */
 
     public boolean overlaps(DateRange other) {
         // TODO: implement date range intersection checking
@@ -44,13 +83,34 @@ public class DateRange {
         }
         return overlaps;
     }
-
+    
+    /**
+     * hashCode method allowing use in collections
+     * 
+     * A method that overrides the normal hashCode() function to facilitate comparisons of 
+     * collections of this object.
+     * 
+     * @return an int specifying which bucket the object should be stored in
+     */
+    
     @Override
     public int hashCode() {
         // hashCode method allowing use in collections
         return Objects.hash(end, start);
     }
-
+    
+    
+    /**
+     * equals method for testing equality in tests
+     * 
+     * A method that overrides the normal equals(), and compares Location objects and assessing 
+     * whether they are equal. This is done by comparing the streetAddress field and the postcode
+     * field.
+     * 
+     * @param  obj a Location that requires comparison
+     * @return     a boolean to indicate whether or not the two locations are equal
+     */
+    
     @Override
     public boolean equals(Object obj) {
         // equals method for testing equality in tests
