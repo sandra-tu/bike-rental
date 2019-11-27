3024ee3525f4d3bc20af92284140ca8638aaecef
Sandra Tu <sandratu2000@gmail.com>
2019-11-27 23:31:54 +0000

    populates Providers in Controller

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.Set;
+import java.util.HashSet;
 import java.util.concurrent.atomic.AtomicLong;
 
 import uk.ac.ed.bikerental.BikeType.BikeTypes;
@@ -17,7 +18,7 @@ public class Bike{
     private BigDecimal replacementValue;
     private BigDecimal dailyRentalPrice;
     private BikeStatuses bikeStatus;
-    private Set<Booking> bookings;
+    private Set<Booking> bookings = new HashSet<Booking>();
     private static AtomicLong idCounter = new AtomicLong();
 
     public Bike(Provider provider, BikeType bikeType) {
