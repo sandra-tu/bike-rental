c0fb3b2d3195d81afc6426bb6afe2f1a09060a73
clairesquires99 <clairesquires99@outlook.com>
2019-11-27 23:36:50 +0000

    Merge changes

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -20,11 +20,7 @@ public class Bike{
     private BigDecimal replacementValue;
     private BigDecimal dailyRentalPrice;
     private BikeStatuses bikeStatus;
-<<<<<<< HEAD
     private Set<Booking> bookings = new HashSet<>();
-=======
-    private Set<Booking> bookings = new HashSet<Booking>();
->>>>>>> 3024ee3525f4d3bc20af92284140ca8638aaecef
     private static AtomicLong idCounter = new AtomicLong();
 
     public Bike(Provider provider, BikeType bikeType) {
