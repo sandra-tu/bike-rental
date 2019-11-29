b0ae61799bd4e062fef2d0183587b7bb98148408
clairesquires99 <clairesquires99@outlook.com>
2019-11-29 14:51:15 +0000

    trying to fix Bike tests

--- tests/uk/ac/ed/bikerental/TestBike.java
+++ tests/uk/ac/ed/bikerental/TestBike.java
@@ -1,68 +1,103 @@
 package uk.ac.ed.bikerental;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Arrays;
-
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-
 import uk.ac.ed.bikerental.BikeType.BikeTypes;
 
 public class TestBike {
     
-    private Bike bike1, bike2, bike3;
-    private Bike[] bikeArr;
-    private Set<Bike> bikeset;
-    private Booking booking1;
-    private Quote quote1;
-    private Provider provider1;
+    private static Bike bike1, bike2, bike3;
+    private static Bike[] bikeArr;
+    private static Set<Bike> bikeset;
+    private static Booking booking1;
+    private static Quote quote1;
+    private static Provider provider1;
+    private static BikeType mountainBike;
+    private static int boop = 1;
     
     @BeforeEach
-    void setUp() {
-        BikeType mountainBike = new BikeType(BikeTypes.MOUNTAINBIKE, new BigDecimal(100.00));
-        this.bike1 = new Bike(provider1, mountainBike);
-        this.bike2 = new Bike(provider1, mountainBike);
-        this.bike3 = new Bike(provider1, mountainBike);
+    public static void setUp() {
+        System.out.println("boo");
+        provider1 = new Provider("Provider", new Location("EH20lklkj", "street"), 
+                new BigDecimal("1.0"));
 
-        this.provider1 = new Provider("Provider", new Location("EH20", "street"), 
-                                       new BigDecimal("1.0"));
-        this.bikeArr = new Bike[]{bike1, bike2, bike3};
-        this.bikeset = new HashSet<>(Arrays.asList(bikeArr));
+        BikeType mountainBike = new BikeType(BikeTypes.MOUNTAINBIKE, new BigDecimal(100.00));
+        System.out.println("boo");
+
+        bike1 = new Bike(provider1, mountainBike);
+        bike2 = new Bike(provider1, mountainBike);
+        bike3 = new Bike(provider1, mountainBike);
         
-        this.quote1 = new Quote(bikeset, provider1, new DateRange(LocalDate.of(2000, 01, 01),
+        System.out.println(bike1.getBikeID());
+        
+        bikeArr = new Bike[]{bike1, bike2, bike3};
+        bikeset = new HashSet<>(Arrays.asList(bikeArr));
+        
+        quote1 = new Quote(bikeset, provider1, new DateRange(LocalDate.of(2000, 01, 01),
                       LocalDate.of(2000, 02, 02)), new Location("EH2020", "quoteLoc"));
+        quote1.setIsPaid(true);
+        booking1 = new Booking(quote1, false);
         
-        this.booking1 = new Booking(quote1, false);
+        //provider1.addProviderBooking(booking1);
+        boop++;
+    }
+    
+    @Test
+    public void testsRunnging() {
+        System.out.println("Tests running");
+        System.out.println(boop);
+
+        //bike1 = new Bike(provider1, mountainBike);
+        //System.out.println("Tests running");
+        System.out.print("Quote null: ");
+        System.out.println(quote1 == null);
+        
+        System.out.print("Bike null: ");
+        System.out.println(bike1 == null);
+        
+        System.out.print("BikeType null: ");
+        System.out.println(mountainBike == null);
+        System.out.println(mountainBike);
+        
+        System.out.print("Provider null: ");
+        System.out.println(provider1 == null);
+        System.out.println(provider1.getProviderID());
+        
+        assertTrue(true);
     }
     
     //Test that the createBikeID method is working correctly
     @Test
     public void testBikeID() {
-        assertEquals((Integer) 1, this.bike1.getBikeID());
-        assertEquals((Integer) 2, this.bike2.getBikeID());
-        assertEquals((Integer) 3, this.bike3.getBikeID());
+        assertEquals((Integer) 1, bike1.getBikeID());
+        assertEquals((Integer) 2, bike2.getBikeID());
+        assertEquals((Integer) 3, bike3.getBikeID());
     }
     
     @Test
     public void testSetBookings() {
-        Set<Booking> bookingSet = new HashSet<>();
-        bookingSet.add(booking1);
-        System.out.println(this.booking1 == null);
-        this.bike1.setBookings(bookingSet);
-        assertEquals(bookingSet, this.bike1.getBookings());
+//        Set<Booking> bookingSet = new HashSet<>();
+//        bookingSet.add(booking1);
+//        System.out.println(" " + booking1 == null);
+//        bike1.setBookings(bookingSet);
+        bike1.addBikeBooking(booking1);
+        assertEquals(booking1, bike1.getBookings());
     }
     
     @Test
     public void testAddBooking() {
         Set<Booking> bookingSet = new HashSet<>();
         bookingSet.add(booking1);
-        this.bike2.addBikeBooking(booking1);
-        assertEquals(bookingSet, this.bike2.getBookings());
+        bike2.addBikeBooking(booking1);
+        assertEquals(bookingSet, bike2.getBookings());
     }
     
 }
